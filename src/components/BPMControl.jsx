589f396ac3d49ca3f3ef4785259d2acb68c1076e
mahtj002 <mahtj002@mymail.unisa.edu.au>
2025-11-15 12:28:16 +1030

    Ready to submit

--- src/components/BPMControl.jsx
+++ src/components/BPMControl.jsx
@@ -2,7 +2,14 @@ function BPMControl(props){
     return(
         <>
             <div className="col d-flex justify-content-center align-items-center">
-                <input type="number" placeholder="BPM" value={props.bpm} style={{ width: '60px', textAlign: 'center' }}
+                <input 
+                type="number" 
+                placeholder="BPM" 
+                value={props.bpm} 
+                style={{ width: '60px', textAlign: 'center' }}
+                step={0.1}
+                min={0.1}
+                max={0.8}
                 onChange={(e) => {
                         const newBPM = e.target.value;
                         props.setBPM(newBPM);
